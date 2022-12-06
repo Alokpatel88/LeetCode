97cacf07b79c35a8d616dd88cdb6a0c690d781f2
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2022-12-06 17:36:05 +0530

    Time: 3 ms (62.81%), Space: 8.5 MB (39.82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,42 +1,35 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
+    private:
+        void inorderHelper(TreeNode *root, vector<int> &v)
+        {
+            if (root)
+            {
+
+                inorderHelper(root->left, v);
+                v.push_back(root->val);
+                inorderHelper(root->right, v);
+
+                return;
+            }
+        }
+
     public:
         vector<int> inorderTraversal(TreeNode *root)
         {
             vector<int> v;
-            stack<TreeNode*> st;
-            if (root == NULL)
-            {
-                return v;
-            }
-            while (true)
-            {
-                if (root != NULL)
-                {
-                    st.push(root);
-                    root = root->left;
-                }
-                else
-                {
-                    if (st.empty())
-                        break;
-                    root = st.top();
-                    v.push_back(root->val);
-                    st.pop();
-                    root=root->right;
-                }
-            }
+            inorderHelper(root, v);
             return v;
 
            	//************recursive**************************//
