17ac6e11a45e184d0bf3adc817cc30db38d72f3d
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2022-12-06 17:11:31 +0530

    Time: 3 ms (62.81%), Space: 8.4 MB (39.82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,54 +1,50 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
-         vector<int>v;
-        if(root==NULL){
+class Solution
+{
+    public:
+        vector<int> inorderTraversal(TreeNode *root)
+        {
+            vector<int> v;
+            stack<TreeNode*> st;
+            if (root == NULL)
+            {
+                return v;
+            }
+            while (true)
+            {
+                if (root != NULL)
+                {
+                    st.push(root);
+                    root = root->left;
+                }
+                else
+                {
+                    if (st.empty())
+                        break;
+                    root = st.top();
+                    v.push_back(root->val);
+                    st.pop();
+                    root=root->right;
+                }
+            }
             return v;
-            
+
+           	//************recursive**************************//
+           	// inorderTraversal(root->left);
+           	// v.push_back(root->val);	// print(root->val)
+           	// inorderTraversal(root->right);
+
+           	//******************************************************//
+
         }
-        stack<TreeNode*>s;
-        TreeNode* curr=root;
-        
-        while(true){
-            if(curr!=NULL){
-                s.push(curr);
-                curr=curr->left;
-            }
-            else{
-                if(s.empty()==true)
-                    break;
-                
-                curr=s.top();
-                s.pop();
-                v.push_back(curr->val);
-                curr=curr->right;
-                
-            }
-        }
-        return v;
-        
-        
-        //************recursive**************************//
-        // inorderTraversal( root->left);
-        // v.push_back(root->val); // print(root->val)
-        // inorderTraversal( root->right);
-        
-        //******************************************************//
-        
-       
-        
-        
-        
-    }
 };
