92b1361f7a28e2fd7195551a1eb88f14f20dbd77
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2022-12-16 01:18:49 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -92,27 +92,48 @@ struct Node
 }; */
 
 class Solution {
-    private:
-    int height(Node*root){
-        if(root==NULL){
-            return 0;
-        }
-        int left=height(root->left);
-        int right=height(root->right);
-        return max(left,right)+1;
-    }
+    // private:
+    // int height(Node*root){
+    //     if(root==NULL){
+    //         return 0;
+    //     }
+    //     int left=height(root->left);
+    //     int right=height(root->right);
+    //     return max(left,right)+1;
+    // }
   public:
+  
+  pair<int,int>DiamFast(Node*root){
+        if(root==NULL){
+            pair<int,int>p=make_pair(0,0);
+            return p;
+        }
+        pair<int ,int>left=DiamFast(root->left);
+        pair<int,int>right=DiamFast(root->right);
+      int  op1=left.first;
+      int  op2=right.first;
+      int  op3=left.second+right.second;
+
+        pair<int,int>ans;
+        ans.first=max(op1,max(op2,op3));
+        ans.second=max(left.second,right.second)+1;
+      
+      return ans;
+    }
+    
+    
     // Function to return the diameter of a Binary Tree.
     int diameter(Node* root) {
         if(root==NULL){
             return 0;
         }
-        int op1=diameter(root->left);
-        int op2=diameter(root->right);
-        int op3=height(root->left)+height(root->right)+1;
+        return DiamFast(root).first+1;
+        // int op1=diameter(root->left);
+        // int op2=diameter(root->right);
+        // int op3=height(root->left)+height(root->right)+1;
 
-        // Your code here
-        return max(op1,max(op2,op3));
+        // // Your code here
+        // return max(op1,max(op2,op3));
     }
 };
         
