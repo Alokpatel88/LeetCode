bda9db5cdc63970a3187f67a35da222caed594d0
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-02-02 00:01:47 +0530

    Added solution - LeetHub

--- Search a node in BST - GFG/search-a-node-in-bst.cpp
+++ Search a node in BST - GFG/search-a-node-in-bst.cpp
@@ -58,17 +58,18 @@ int main() {
 // Function to search a node in BST.
 bool search(Node* root, int x) {
     // Your code here
-    while(root!=NULL){
-        if(root->data==x){
-            return true;
-        }
-        if(root->data<x){
-            root=root->right;
-            
-        }
-        else{
-            root=root->left;
-        }
-    }
-    return false;
+  if(root==NULL){
+      return false;
+      
+  }
+  if(root->data==x){
+      return true;
+      
+  }
+  if(root->data>x){
+      return search(root->left,x);
+  }
+  else
+  return search(root->right,x);
+  
 };
