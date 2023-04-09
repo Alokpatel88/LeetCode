e0f8d423e0d87acce2f56144ea857f8efe14e3d8
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-04-10 00:45:33 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i=0;i<nums.size() - 1;i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[i] + nums[j]==target)
-                {
-                    // vector<int>temp;
-                    // temp.push_back(min(nums[i],nums[j]));
-                    // temp.push_back(max(nums[i],nums[j]));
-                    // ans.push_back(temp);
-                        ans.push_back(i);
-                        ans.push_back(j);
-                        break;
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int req=target-nums[i];
+            if(mp.find(req)!=mp.end()){
+                ans.push_back(i);
+                ans.push_back(mp[req]);
 
-                }
-                
-              }
-         }
- 
+            }
+            mp[nums[i]]=i;
+        }
         return ans;
     }
 };
