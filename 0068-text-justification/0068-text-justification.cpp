7507de152901c37e3695c141e791736359728f03
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-08-25 02:12:40 +0530

    Time: 0 ms (100%), Space: 7.6 MB (5.93%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,44 +1,45 @@
-class Solution // 100% faster
-{
+class Solution {
 public:
-    vector<string> fullJustify(vector<string> &words, int maxWidth)
-    {
-        // Vector to store the sentences
+    vector<string> fullJustify(vector<string>& words, int width) {
+        int length=0;  // to store the length of words in current line
+        vector<string> temp;
         vector<string> ans;
-       int curr_len = 0, start = 0, end = -1;
-        for (string s : words)
-        {
-            if ((end - start + 1) + curr_len + s.size() <= maxWidth)
-                curr_len += s.size(), end++;
-            else
-            {
-                string sentence = words[start];
-               long long int extra_space = maxWidth - curr_len, padding = extra_space / max(1, end - start);
-              
-                int extra = extra_space % max(1, end - start);
-                 for (int i = start + 1; i <= end; i++)
-                {
-                    for ( long long int j = 0; j < padding; j++)
-                        sentence += " ";
-                    // Add the xtra padding, if needed
-                    if (extra)
-                        sentence += " ", extra--;
-                    sentence += words[i];
+        for(auto it: words){
+            if(length + temp.size() + it.length() <= width){
+                temp.push_back(it);
+                length+=it.length();
+            }
+            else{
+                string line=temp[0]; //storing first word of the line
+                int spaces=width-length;
+                if(temp.size()==1){
+                    line+=string(spaces,' ');
                 }
-               
-                while (sentence.size() < maxWidth)
-                    sentence += " ";
-                ans.push_back(sentence);
-               start = end + 1, curr_len = s.size(), end++;
+                else{
+                    int wordspace=spaces/ (temp.size()-1); //spaces needed before every word
+                    int extra=spaces % (temp.size()-1);
+                    for(int i=1;i<temp.size();i++){
+                        
+                        line+=string(wordspace,' ');
+                        
+                        if(extra > 0) {line+=' '; extra--;}  //assigning extra space to left
+                        
+                        line+=temp[i];
+                    }
+                }
+                ans.push_back(line);
+                temp.clear();
+                temp.push_back(it);
+                length=it.length();
             }
         }
-       
-        string sentence = words[start];
-        for ( long long int i = start + 1; i <= end; i++)
-            sentence += " " + words[i];
-        while (sentence.size() < maxWidth)
-            sentence += " ";
-        ans.push_back(sentence);
+        string last=temp[0]; //for last word
+        for(int i=1;i<temp.size();i++){
+            last+=' ';
+            last+=temp[i];
+        }
+        last+=string(width-last.length(),' ');
+        ans.push_back(last);
         return ans;
     }
 };
