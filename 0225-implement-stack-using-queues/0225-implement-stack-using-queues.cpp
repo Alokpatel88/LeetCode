d7c30065b08c3e9061222eda5fd865a392531e59
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-08-28 21:56:31 +0530

    Time: 0 ms (100%), Space: 6.7 MB (67.22%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,44 +1,25 @@
 class MyStack {
 public:
-    queue<int> q1, q2;
-    
-    /** Initialize your data structure here. */
     MyStack() {
-        
-    }
     
-    /** Push element x onto stack. */
+    }
+    queue<int> que;
     void push(int x) {
-        q1.push(x);
+        que.push(x);
+		for(int i=0;i<que.size()-1;++i){
+			que.push(que.front());
+			que.pop();
+		}
     }
-    
-    /** Removes the element on top of the stack and returns that element. */
     int pop() {
-        while(q1.size() > 1){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        //last element in q1
-        int x = q1.front(); q1.pop();
-        q1.swap(q2);
+        int x=que.front();
+		que.pop();
         return x;
     }
-    
-    /** Get the top element. */
     int top() {
-        return q1.back();
+        return que.front();
     }
-    
-    /** Returns whether the stack is empty. */
     bool empty() {
-        return q1.empty();
+        return que.empty();
     }
 };
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
