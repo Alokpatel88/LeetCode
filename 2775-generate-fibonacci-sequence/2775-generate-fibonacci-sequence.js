bd0c73a212cc0fd5de009dc1d6afb73adab883e2
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-09-06 23:51:08 +0530

    Time: 51 ms (63.5%), Space: 42.1 MB (39.24%) - LeetHub

---
+++ 2775-generate-fibonacci-sequence/2775-generate-fibonacci-sequence.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Generator<number>}
+ */
+function* fibGenerator() {
+    let a = 0;
+    let b = 1;
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+}
+
+// Usage example:
+const gen = fibGenerator();
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next().value); // 3
+// ... and so on
