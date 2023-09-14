334ebf34df46e485a38176c822f76f108bf764fd
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-09-14 09:56:30 +0530

    Time: 23 ms (65.12%), Space: 10.5 MB (60.49%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -3,23 +3,28 @@ public:
     string removeDuplicates(string s, int k) {
         stack < pair<char,int> > st;
         for(int i =0; i< s.size() ; i++){
-             if(!st.empty() && st.top().first == s[i]){
+        //      if(!st.empty() && st.top().first == s[i]){
+        //         st.top().second++;
+        //         if( st.top().second == k){
+        //             st.pop();
+        //         }
+        //     }
+        //        else{
+        //         st.push({s[i],1});
+        //     }
+        // }
+        if(!st.empty() && st.top().first == s[i]){
                 st.top().second++;
-                if( st.top().second == k){
-                    st.pop();
-                }
-            }
-               else{
-                st.push({s[i],1});
-            }
-        }
-            //  if( !st.empty() && st.top().second == k){
-            //     st.pop();
             
-            // if(!st.empty() && st.top().first == s[i]){
-            //     st.top().second++;
-            // }
-            //  }
+             if( !st.empty() && st.top().second == k){
+                st.pop();
+            
+             }
+        }
+        else{
+            st.push({s[i],1});
+           }
+        }
          
         
         string ans = "";
