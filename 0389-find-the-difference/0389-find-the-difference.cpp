70e62039f23c92b6ca1e03e1db5ea819a7061bc1
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-09-25 16:27:47 +0530

    Time: 4 ms (48.11%), Space: 6.9 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char dig = '\0';
-        for(int i = 0; i< s.size(); i++){
-            dig = dig^s[i]; 
-        }
-        for(int i = 0; i< t.size(); i++){
-            dig = dig^t[i]; 
-        }
-        return dig;
+       char res = '\0';
+       for(auto it : s) res^= it;
+       for(auto it :t) res ^= it;
+       return res;
     }
 };
