0e9303180a2f46fced028873070c33c3ff57b95a
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-10-05 23:54:59 +0530

    Time: 8 ms (68.48%), Space: 16.3 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         map<int,int> mp;
-        int n= nums.size();
+        int n = nums.size();
         for(auto it: nums){
             mp[it]++;
         }
