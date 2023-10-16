ec6cf1a117fc6f0907ca499cd3faffc4c981221f
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-10-16 19:43:59 +0530

    Time: 0 ms (100%), Space: 6.7 MB (35.86%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    
     vector<int> getRow(int n) {
         n = n + 1;
-        vector<int> ans;
+        vector<int > ans;
         ans.push_back(1);
-        long long num = 1;
-        for(int i = 1; i < n  ; i++){
-            num = num * (n - i);
-            num = num / i;   
+        long long num  = 1;
+        for(int i = 1 ; i < n ; i++){
+            num = num * ( n -  i );
+            num = num / i;
             ans.push_back(num);
+
         }
         return ans;
+        
     }
 };
