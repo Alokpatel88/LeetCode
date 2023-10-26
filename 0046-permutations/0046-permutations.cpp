9fe4ab26d9f1029144d011e0088cdadfadb0a734
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-10-26 18:48:21 +0530

    Time: 3 ms (54.05%), Space: 7.9 MB (78.07%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,25 @@
 class Solution {
     private:
-    void genrate(vector<int>nums, vector<vector<int>> &ans, int i){
-        if(i >= nums.size()){
+    void solve(vector<int>nums, vector<vector<int>> &ans, int i ){
+        //base case
+        if(i>=nums.size()){
+            ans.push_back(nums);
             return ;
         }
-        for(int j = i; j< nums.size(); j++){
-            ans.push_back(nums);
-            swap(nums[i], nums[j]);
         
-            genrate(nums, ans, i+1 );
+        for(int j = i ; j < nums.size(); j++){
+            swap(nums[i], nums[j]);
+            
+            solve(nums, ans,  i+1);
+            // backtracking
             swap(nums[i], nums[j]);
         }
-
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans,ans1;
-        genrate(nums, ans, 0);
-        set<vector<int>> st;
-        for(auto it : ans){
-            st.insert(it);
-        }
-        for(auto it : st){
-            ans1.push_back(it);
-        }
-        return ans1;
+        vector<vector<int>> ans;
+        int i = 0;
+        solve(nums, ans, i);
+        return ans;
     }
 };
