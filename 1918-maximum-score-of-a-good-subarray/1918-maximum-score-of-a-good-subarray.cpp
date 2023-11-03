037deb95bd5d874caf5b97d3d5049f77afaa5151
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2023-11-04 00:49:02 +0530

    Time: 138 ms (36.53%), Space: 89.7 MB (96.74%) - LeetHub

---
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumScore(vector<int>& A, int k) {
+        int i = k - 1, j = k + 1, N = A.size(), ans = A[k], mn = A[k];  
+        for (int len = 2; len <= N; ++len) {
+            int lmin = min(mn, i >= 0 ? A[i] : 0); 
+            int rmin = min(mn, j < N ? A[j] : 0); 
+            if (lmin >= rmin) --i; 
+            else ++j;
+            mn = max(lmin, rmin);
+            ans = max(ans, mn * len);
+        }
+        return ans;
+    }
+};
