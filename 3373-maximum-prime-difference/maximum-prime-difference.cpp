fc9365a55e09386fd1db452dc9a38734b4acd21b
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2024-04-20 23:23:43 +0530

    Time: 192 ms (5.04%) | Memory: 106.8 MB (95.50%) - LeetSync

---
+++ 3373-maximum-prime-difference/maximum-prime-difference.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+bool isprime(int n){
+  
+        // cout<< " *"<<" ";
+        if(n==1) return false;
+        if(n == 2) return true;
+        
+   
+    for(int i = 2; i * i <= n; i++){
+        if(n % i == 0){
+            cout<< " *"<<" ";
+            return false;
+            
+        }
+
+    }
+    // cout<< " *" <<" ";
+    return true;
+}
+    
+    int maximumPrimeDifference(vector<int>& nums) {
+        int f = INT_MAX;
+        int s = INT_MIN;
+        for(int i = 0; i< nums.size(); i++){
+            if(isprime(nums[i])){
+                if(i < f){
+                    f = i;
+                }
+                if( i > s){
+                    s = i;
+                }
+            }
+        }
+        return abs(s-f);
+    }
+};
