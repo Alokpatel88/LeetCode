bd40d808479597ea8246acd87f585bbafa976fb6
Alok Patel <91893516+Alokpatel88@users.noreply.github.com>
2025-02-09 00:24:26 +0530

    Time: 105 ms (12.68%) | Memory: 18.6 MB (40.11%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // don't modify actual Data;
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2; 
+        ListNode* newHead = new ListNode(); 
+        ListNode* newtemp = newHead;
+        if(temp1 == nullptr ){
+            return temp2;
+        }
+         if( temp2 == nullptr){
+            return temp1;
+        }
+
+        while(temp1  != NULL && temp2 != NULL){
+            if(temp1 -> val <= temp2->val){
+                newtemp->next = temp1;
+                temp1 = temp1->next;
+            }
+
+            else{
+                newtemp->next = temp2;
+                temp2 = temp2->next;
+                // cout<<" 2 ";
+            }
+            newtemp = newtemp-> next;
+        }
+        if(temp1){
+            newtemp->next = temp1; 
+        }
+
+        if(temp2){
+           newtemp->next = temp2;
+        }
+        return newHead->next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* list1= new ListNode();
+        ListNode* list2 = list1->next;
+        for(int i = 0; i< lists.size(); i++){
+          list2 =   mergeTwoLists(list2, lists[i]);
+           
+        }
+        return list2;
+
+    }
+};
